50fedc891602f205b1f0758fbe2712985603ac20
minnuss <ponjevic.goran@hotmail.com>
2021-05-06 18:30:44 +0200

    minor fixes

--- js/main.js
+++ js/main.js
@@ -17,14 +17,18 @@ function calculate() {
     // console.log(currency_one, currency_two)
 
     async function getExchangeRates() {
+        // fetching currency one
         const res = await fetch(url + currency_one)
         const data = await res.json()
 
         // console.log(dataOne)
+
+        // getting the rates from currency one data for currency two
         const rate = data.rates[currency_two]
         // rate element
         rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`
-        // amount equation
+        // amount equation for second input element
+        // value of input one multiplied by rate
         amountTwoInput.value = (amountOneInput.value * rate).toFixed(2)
     }
     getExchangeRates()
@@ -36,11 +40,15 @@ amountOneInput.addEventListener('input', calculate)
 currencyTwo.addEventListener('change', calculate)
 amountTwoInput.addEventListener('input', calculate)
 
-// SWAP BUTTON
+// SWAP BUTTON FUNCTIONALITY
 btnSwap.addEventListener('click', () => {
+    // put select value of currency one to temp variable
     const temp = currencyOne.value
+    // then change currency one value to value of currency two
     currencyOne.value = currencyTwo.value
+    // and then get the currency two value from temp
     currencyTwo.value = temp
+    // call calculate function when swap is complete
     calculate()
 })
 
